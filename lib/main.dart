a919a8d941324b20d4e108f7b8769c48588868ff
FaithNnakwe <ujunwannakwe@gmail.com>
2025-02-05 13:37:09 -0500

    Implemented play feature for digital pet

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MyApp()); // Example 1: Create Repository for Git Collab.
 }
 
 class MyApp extends StatelessWidget {
